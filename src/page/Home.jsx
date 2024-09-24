904761ebd68d6e4aa4f9c8e9a2bbaded911ed375
sailendraprasath <sailendraprasath05@gmail.com>
2024-09-24 23:40:56 +0530

    i did card and slider then i will do make this home page properly

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,6 +1,6 @@
 import Cards from "../components/Cards";
 import Navbar from "../components/Navbar";
-import SlideCard from "../components/SlideCards";
+// import SlideCard from "../components/SlideCards";
 // import Carousel from "../components/Carousel";
 // import Card from "../components/Card";
 import Welcome from "../components/Welcome";
@@ -12,7 +12,7 @@ const Home = () => {
       <div>
         <Navbar />
         <Welcome />
-        <SlideCard />
+        {/* <SlideCard /> */}
         <Cards />
         <FooterShop />
         {/* <Carousel /> */}
